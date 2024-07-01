772565076e477a2db9f1e9608a4d7868823a38ea
antyytna <adetrani123@gmail.com>
2024-07-01 12:54:10 -0400

    movement2

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,6 +12,9 @@ var endlag
 var walking
 var dashing = false # Refers to the secondary dash state, not regular walking. 
 var idle
+var basic_attack
+var spin_attack
+var should_handle_endlag
 
 
 func _ready():
@@ -19,7 +22,14 @@ func _ready():
 	endlag = false
 
 func _physics_process(delta):
-	print(velocity.x)
+	
+	if velocity.x < 0:
+		$Sprite2D.flip_h = true
+	elif velocity.x > 0:
+		$Sprite2D.flip_h = false
+	
+	if idle or walking == true:
+		$AnimationPlayer.play("default")
 	if idle == true:
 		SPEED = 300.0
 	
@@ -37,7 +47,7 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 		
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction and endlag == false:
+	if direction:
 		var walking = true
 		velocity.x = direction * SPEED
 	else:
@@ -51,5 +61,43 @@ func _physics_process(delta):
 		idle = true
 	else:
 		idle = false
+	
+	if is_on_floor() and endlag == false and $BasicAttackTimer.time_left == 0:
+		if Input.is_action_just_pressed("attack"):
+			should_handle_endlag = true
+			basic_attack = true
+			endlag = true
+			$AnimationPlayer.play("attack")
+			
+	if in_air == true and endlag == false and Input.is_action_just_pressed("attack"):
+		should_handle_endlag = true
+		spin_attack = true
+		endlag = true
+		$AnimationPlayer.play("spin")
+	
+	if should_handle_endlag == true:
+		if endlag == true:
+			should_handle_endlag = false
+			handle_endlag()
 		
 	move_and_slide()
+
+func handle_endlag():
+	idle = false
+	if basic_attack == true:
+		$BasicAttackTimer.start()
+	
+	if spin_attack == true:
+		$SpinAttackTimer.start()
+
+
+func _on_basic_attack_timer_timeout():
+	endlag = false
+	basic_attack = false
+	idle = true
+
+
+func _on_spin_attack_timer_timeout():
+	endlag = false
+	spin_attack = false
+	idle = true
